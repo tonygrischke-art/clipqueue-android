295f92bad8b9a758c59de6990bba29186c3abd24
Tony Grischke <tony@grischke.art>
2026-07-21 01:55:42 -0400

    fix: use SingletonComponent for Hilt module; increase Gradle heap for dex merge

--- app/src/main/java/com/aetheria/clipqueue/di/RepositoryModule.kt
+++ app/src/main/java/com/aetheria/clipqueue/di/RepositoryModule.kt
@@ -6,12 +6,12 @@ import com.aetheria.clipqueue.data.ClipQueueRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
 
     @Provides
